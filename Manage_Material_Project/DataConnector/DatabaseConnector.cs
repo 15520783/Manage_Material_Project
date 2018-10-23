9d6c231ec4542d56cf07e5f79be7a3072b9d5ef1
Thái Nguyễn <15520783@gm.uit.edu.vn>
2018-10-23 21:23:50 +0700

    Create project
    Signed-off-by: Thái Nguyễn <15520783@gm.uit.edu.vn>

---
+++ Manage_Material_Project/DataConnector/DatabaseConnector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manage_Material_Project.DataSource
+{
+    class DatabaseConnector
+    {
+        private const string connectionString = "someone";
+
+        SqlConnection conn;
+
+
+        public void Connect()
+        {
+            this.conn = new SqlConnection(connectionString);
+            this.conn.Open();
+        }
+
+        public void Disconnect()
+        {
+            this.conn.Close();
+        }
+    }
+
+}
