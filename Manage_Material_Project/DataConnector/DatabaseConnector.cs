18d79dbe0bfa3671bd0ff76023ea2537282601ae
Duc Quang <phanducquang07@gmail.com>
2018-11-15 22:38:11 +0700

    thêm hàm getdata

--- Manage_Material_Project/DataConnector/DatabaseConnector.cs
+++ Manage_Material_Project/DataConnector/DatabaseConnector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace Manage_Material_Project.DataSource
 {
     class DatabaseConnector
     {
-        private const string connectionString = "someone";
+        private const string connectionString = "Server=tcp:dbwp.database.windows.net,1433;Initial Catalog=db;Persist Security Info=False;User ID=dbwp@dbwp;Password=Dev11111111;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         SqlConnection conn;
 
@@ -23,6 +24,18 @@ namespace Manage_Material_Project.DataSource
         public void Disconnect()
         {
             this.conn.Close();
+        }
+
+        private DataTable Getdata(string QuerrySQL)
+        {
+            Connect();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = QuerrySQL;
+            SqlDataAdapter sda = new SqlDataAdapter(cmd.CommandText, conn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            Disconnect();
+            return dt;
         }
     }
 
